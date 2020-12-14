31804c0818f8829f900089efc49b76a65844f846
ceprio <c.git@pronovost.net>
2020-12-14 16:09:17 -0500

    wip

--- flexxamples/ui_usage/combo_box.py
+++ flexxamples/ui_usage/combo_box.py
@@ -2,44 +2,28 @@
 Simple use of a dropdown,
 """
 
+
 from flexx import app, event, ui, flx
+
 
 class Example(ui.Widget):
 
     def init(self):
         # A combobox
         self.combo = ui.ComboBox(editable=True,
-                                 options=('foo', 'bar', 'spaaaaaaaaam', 'eggs'))
+                                 options=('foo', 'bar', 'spaaam', 'eggs', 'bacon'))
         self.label = ui.Label()
+        self.button = ui.Button(text='change list')
 
-#         @event.connect('combo.text')
-#         def on_combobox_text(self, *events):
-#             self.label.text = 'Combobox text: ' + self.combo.text
-#             if self.combo.selected_index is not None:
-#                 self.label.text += ' (index %i)' % self.combo.selected_index
-
-if __name__ == '__main__':
-     m = flx.launch(Example, 'default-browser')
-     flx.start()
-"""
-Simple use of a dropdown,
-"""
-
-from flexx import app, event, ui, flx
-
-class Example(ui.Widget):
-
-    def init(self):
-        # A combobox
-        self.combo = ui.ComboBox(editable=True,
-                                 options=('foo', 'bar', 'spaaaaaaaaam', 'eggs'))
-        self.label = ui.Label()
-
-#         @event.connect('combo.text')
-#         def on_combobox_text(self, *events):
-#             self.label.text = 'Combobox text: ' + self.combo.text
-#             if self.combo.selected_index is not None:
-#                 self.label.text += ' (index %i)' % self.combo.selected_index
+    @event.reaction('combo.text')  
+    def on_combobox_text(self, *events):
+        text = f'"{self.combo.text}" (index {self.combo.selected_index})'
+        self.label.set_text(text)
+        
+    @event.reaction('button.pointer_click') 
+    def on_button_press(self, *events): 
+        self.combo.set_selected_index(-1)
+        self.combo.set_options((1,2,3))
 
 if __name__ == '__main__':
      m = flx.launch(Example, 'default-browser')
